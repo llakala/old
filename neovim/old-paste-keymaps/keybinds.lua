db4f84fc042e7afc57f2a110fffcde54a601d0bc
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-08-15 13:54:02 -0400

    neovim/old-paste-keymaps: init
    These end up being more frustrating than they are helpful.

---
+++ neovim/old-paste-keymaps/keybinds.lua
@@ -0,0 +1,5 @@
+-- Paste and format, Means we can paste something and it'll be properly
+-- indented! Referenced from:
+-- https://github.com/ku1ik/vim-pasta#why-is-it-better-than-nnoremap-p-pv-
+nnoremap("p", "p`[v`]=")
+nnoremap("P", "P`[v`]=")
